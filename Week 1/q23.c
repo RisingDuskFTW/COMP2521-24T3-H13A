71aad61d057420861dbbcb2fc6c6ff94a5a564c9
Ryan Berlee <z5358674@ad.unsw.edu.au>
2024-09-11 14:35:48 +1000

    Week 1 Solutions

--- Week 1/q23.c
+++ Week 1/q23.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct node {
     int value;
@@ -15,5 +16,29 @@ struct list {
 
 // Solution:
 void listDelete(struct list *l, int value) {
-    // TODO
+    // Empty list
+    if (l->head == NULL) {
+        return;
+    }
+
+    // Delete the first element
+    if (l->head->value == value) {
+        struct node *temp = l->head;
+        l->head = l->head->next;
+        free(temp);
+        return;
+    }
+
+    // General Case
+    struct node *curr = l->head;
+    struct node *prev = NULL;
+    while (curr != NULL) {
+        if (curr->value == value) {
+            prev->next = curr->next;
+            free(curr);
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
 }
