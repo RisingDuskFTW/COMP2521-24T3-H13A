ebd76a5bb0b053060cf2bf73067343f4063664da
Ryan Berlee <z5358674@ad.unsw.edu.au>
2024-09-12 14:47:51 +1000

    Week 1 Tutorial files

--- Week 1/q23.c
+++ Week 1/q23.c
@@ -16,4 +16,31 @@ struct list {
 // Solution:
 void listDelete(struct list *l, int value) {
     // TODO
+
+    // case 1: if the list is empty
+    if (l->head == NULL) {
+        return;
+    }
+
+    // if the value is the first one in the list
+    if (l->head->value == value) {
+        struct node *temp = l->head;
+        l->head = l->head->next;
+        free(temp);
+        return;
+    }
+
+    // if the value is somewhere else in the list
+    struct node *curr = l->head;
+    while (curr->next != NULL) {
+        if (curr->next->value == value) {
+            struct node *temp = curr->next;
+            curr->next = curr->next->next;
+            free(temp);
+            return;
+        }
+        curr = curr->next;
+    }
+
+    // value is not in the list
 }
