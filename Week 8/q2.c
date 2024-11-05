0925d2cdb6a8d194b7a14804969874eda1720125
Ryan Berlee <z5358674@ad.unsw.edu.au>
2024-11-05 12:02:15 +1100

    Week 8 Tutorial

--- Week 8/q2.c
+++ Week 8/q2.c
@@ -2,20 +2,23 @@
 #include "Set.h"
 #include "Queue.h"
 
-// Solution 1: Using a BFS
-Set reachableBFS(Graph g, Vertex src) {
-	// TODO
-	return NULL;
+static void doRecursiveDFS(Graph g, Vertex v, Set seen);
+
+Set reachable(Graph g, Vertex src) {
+	Set seen = SetNew();
+	doRecursiveDFS(g, src, seen);
+	return seen;
 }
 
-static void doReachable(Graph g, Vertex v, Set seen);
+static void doRecursiveDFS(Graph g, Vertex v, Set seen) {
+	SetAdd(seen, v);
 
-// Solution 2: Using Recursive DFS
-Set reachableDFS(Graph g, Vertex src) {
-	// TODO
-	return NULL;
-}
-
-static void doReachable(Graph g, Vertex v, Set seen) {
-	// TODO
+	// for each vertex
+	for (Vertex w = 0; w < GraphNumVertices(g); w++) {
+		// check if it is adjacent
+		if (GraphIsAdjacent(g, v, w) && !SetContains(seen, w)) {
+			// traverse to that vertex
+			doRecursiveDFS(g, w, seen);
+		}
+	}
 }
